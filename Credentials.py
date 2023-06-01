0b65b5a44a691c03aeb66063c4a87ed300b7733a
Mohammad Hassan Turi <87395528+hassan-turi@users.noreply.github.com>
2023-06-01 16:41:49 +0500

    Update Credentials.py

--- Credentials.py
+++ Credentials.py
@@ -1,11 +1,11 @@
 from azure.common.credentials import ServicePrincipalCredentials
 from azure.identity import ClientSecretCredential
 
-credential = ServicePrincipalCredentials(client_id='ef13c065-188e-4f54-87d1-d15f2b046130',
-                                         secret='WtM8Q~uM0erSC7kUhhvGwFVpZUSokSXdxrgXbda4', tenant='75df096c-8b72-48e4-9b91-cbf79d87ee3a')
+credential = ServicePrincipalCredentials(client_id='XXXXXX',
+                                         secret='WtM8Q~XXXXXX', tenant='XXXXXX')
 
-subscription_id = "0837638e-463c-483c-a424-07651b04c67a"
+subscription_id = "XXXXXX"
 
 
-acredential = ClientSecretCredential(client_id='ef13c065-188e-4f54-87d1-d15f2b046130',
-                                         client_secret='WtM8Q~uM0erSC7kUhhvGwFVpZUSokSXdxrgXbda4', tenant_id='75df096c-8b72-48e4-9b91-cbf79d87ee3a')
+acredential = ClientSecretCredential(client_id='XXXXXX',
+                                         client_secret='XXXXXX', tenant_id='XXXXXX')
